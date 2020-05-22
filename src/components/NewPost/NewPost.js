0af248ae4aa78e9fccc905c1895c3c38940bc076
Ketna20 <55664056+Ketna20@users.noreply.github.com>
2020-05-21 19:50:34 -0700

    getting posts data from jsonplaceholder

---
+++ src/components/NewPost/NewPost.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import classes from './NewPost.module.css';
+
+
+class NewPost extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            content: '',
+            author: 'KK'
+        }
+    }
+    
+    
+    render () {
+        return (
+            <div className={classes.NewPost}>
+                <h1>Add a Post</h1>
+                <label>Title</label>
+                <input type="text" value={this.state.title} 
+                    onChange={(event) => {this.setState({title: event.target.value})}} />
+                <label>Content</label>
+                <textarea rows="4" value={this.state.content} 
+                   onChange={(event) => {this.setState({content: event.target.value})}} />
+                <label>Author</label>
+                <select value={this.state.author} 
+                    onChange={(event) => {this.setState({author: event.target.value})}}>
+                        <option value="KK">KK</option>
+                        <option value="K2">K2</option>
+                </select>
+                <button>Add Post</button>
+            </div>
+        );
+    }
+}
+
+export default NewPost;
---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import axios from 'axios';
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import classes from './Blog.module.css';
+
+class Blog extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            posts: []
+        }
+    }
+    
+    componentDidMount () {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts =  posts.map(post => {
+                    return {
+                        ...post, 
+                        author: 'KK'
+                    }
+                });
+                this.setState({posts: updatedPosts});
+                console.log(response);
+            });
+    }
+    
+    render () {
+        const posts = this.state.posts.map(post => {
+            return <Post key={post.id} title={post.title} author={post.author} />
+        });
+        return (
+            <div>
+                <section className={classes.Posts}>
+                    {posts}
+                </section>
+                <section>
+                    <FullPost />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
